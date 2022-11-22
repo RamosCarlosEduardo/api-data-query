d3c53e4a2f95f143c4aae8110adef461ab89108c
RamosCarlosEduardo <carlosramos.engenharia@gmail.com>
2022-11-22 17:04:36 -0300

    finishing cep autocomplete

--- js/main.js
+++ js/main.js
@@ -1,3 +1,44 @@
+async function buscaEndereco(cep){
+    const elError = document.getElementById('error')
+    elError.innerHTML = ''
+
+    try {
+        let response = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
+        let responseJson = await response.json()
+        
+        if (responseJson.erro) {
+            throw Error('Cep nao existente.')
+        }
+                 
+        return responseJson
+
+    } catch (error) {
+        console.log(error)
+        elError.innerHTML = `<p>Cep inválido, tente novamente.</p>`
+    }
+
+}
+
+cep.addEventListener('focusout', async function (e) {
+    const elEndereco = document.getElementById('endereco')
+    const elBairro = document.getElementById('bairro')
+    const elCidade = document.getElementById('cidade')
+    const elComplemento = document.getElementById('complemento')
+    const elNumero = document.getElementById('numero')
+    const elEstado = document.getElementById('estado')
+    
+    const objEndereco = await buscaEndereco(this.value)
+    
+    elEndereco.value = objEndereco?.logradouro ?? ''
+    elBairro.value = objEndereco?.bairro ?? ''
+    elCidade.value = objEndereco?.localidade ?? ''
+    elComplemento.value = objEndereco?.complemento ?? ''
+    elNumero.value = objEndereco?.numero ?? ''
+    elEstado.value = objEndereco?.uf ?? ''
+    
+})
+
+
 // const urlAPI = (cep) => {return `https://viacep.com.br/ws/${cep}/json/`}
 
 // const result = fetch(urlAPI(27542090))
@@ -11,24 +52,6 @@
 //     })
 //     .catch(err => {console.log(err)})
 //     .finally(msg => console.log('Processamento concluido'))
-    
-
-async function buscaEndereco(cep){
-    try {
-        let consulta = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
-        let consultaConvertida = await consulta.json()
-
-        if (consultaConvertida.erro) {
-            throw Error('Cep nao existente')
-        }
-        return consultaConvertida
-    } catch (error) {
-        console.log(error)
-    }
-    
-}
-
-buscaEndereco(27514090).then(r => console.log(r))
 
 
 //usando vários ceps e Promise.all
